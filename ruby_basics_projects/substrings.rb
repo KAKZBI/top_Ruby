07eb0e277996291d07e81902a978dcd98c4ab389
KAKZBI <enelibrice@yahoo.fr>
2023-12-04 11:42:15 +0100

    variable names modified for better readability in substrings.rb

--- ruby_basics_projects/substrings.rb
+++ ruby_basics_projects/substrings.rb
@@ -1,11 +1,13 @@
 def substrings(string, dictionary)
     hash = {} 
     #Create an array containing valid substrings
-    arr = dictionary.select{|substring| string.downcase.include?(substring.downcase)}
-    #Foe each valid string make it a key in our hash and as value give it the number of times it appear in our string
-    arr.each do |substring|
-        value = string.downcase.split.select{|word| word.include?(substring)}.length
-        hash[substring] = value
+    valid_substrings = dictionary.select{|substring| string.downcase.include?(substring.downcase)}
+    # p valid_substrings, string.split
+    #For each valid string make it a key in our hash and as value give it the 
+    # number of times it appear in our string
+    valid_substrings.each do |substring|
+        substring_occurences = string.downcase.split.select{|word| word.include?(substring)}.length
+        hash[substring] = substring_occurences
     end
     return hash
 end
